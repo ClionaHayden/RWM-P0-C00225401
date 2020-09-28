31b8063fa1093481f0f502cf6ae9be0841fa3903
Cliona Hayden <c00225401@itcarlow.ie>
2020-09-28 12:30:39 +0100

    0.2.0 added factorial calcuation and test

--- RWM-P0-C00225401/Assets/Scripts/CubeFilter.cs
+++ RWM-P0-C00225401/Assets/Scripts/CubeFilter.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
 public class CubeFilter
 {
     public static int cube(int x)
@@ -12,6 +15,32 @@ public class CubeFilter
         for (int i = 0; i < xs.Length; i++)
         {
             result[i] = cube(xs[i]);
+        }
+        return result;
+    }
+
+    public static int[] fact(int[] xs)
+    {
+        int[] result = new int[xs.Length];
+        for (int i = 0; i < xs.Length; i++)
+        {
+            int multiply;
+            // check for 0
+            if (xs[i] > 1)
+            {
+                multiply = xs[i] - 1;
+            }
+            else
+            {
+                multiply = xs[i];
+            }
+
+            // calcuate the factorial
+            for (; multiply > 0; multiply--)
+            {
+                xs[i] = xs[i] * multiply;
+            }
+            result[i] = xs[i];
         }
         return result;
     }
--- RWM-P0-C00225401/Assets/Tests/CubeFilterTest.cs
+++ RWM-P0-C00225401/Assets/Tests/CubeFilterTest.cs
@@ -19,6 +19,21 @@ namespace Tests
 
             CollectionAssert.AreEqual(expected, output);
         }
+
+        [Test]
+        public void CubeFilterFact()
+        {
+            int[] input = { 4, 2, 8, 3, 9, 4, 10 };
+            int[] output = CubeFilter.fact(input);
+            int[] expected = { 24, 2, 40320, 6, 362880, 24, 3628800 };
+
+            foreach (var o in output)
+            {
+                Debug.Log(o);
+            }
+
+            CollectionAssert.AreEqual(expected, output);
+        }
     }
 }
 
